4f475800836f1901cf817012f69248757a441d51
duonqHoang <duonghoang.v06@gmail.com>
2023-08-22 14:40:47 +0700

    fix env

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,7 +2,7 @@ import { DataSource } from "typeorm";
 
 const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
+  host: process.env.MYSQL_HOST,
   port: +process.env.MYSQL_PORT,
   username: process.env.MYSQL_USERNAME,
   database: "hapolearn",
