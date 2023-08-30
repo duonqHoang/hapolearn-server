3c2c8c6fdeef24e37b615008cbd780b4ba9dd77b
duonqHoang <duonghoang.v06@gmail.com>
2023-08-30 08:41:49 +0700

    cors config

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ import router from "./routes";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
